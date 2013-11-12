ba57fb6c7ae00cc07cc1ae0c1f10b589da9ec810
Kent McNeill <kmcneill@Stasis-by-Kent.local>
2013-11-12 12:39:08 +1100

    resolve JSHint issues

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var config = {};
 var fs = require('fs');
 
@@ -121,19 +123,19 @@ config.simplemocha = {
 config.heroku = {
   prereq: function(grunt) {
     if (!fs.existsSync('aws.json')) {
-      grunt.fail.fatal("You need 'aws.json'. Get it from the JetPets MyTW group.");
+      grunt.fail.fatal('You need \'aws.json\'. Get it from the JetPets MyTW group.');
     }
   },
 
   setConfig: function(grunt, signal) {
     var system = createSystem(grunt);
     var aws = require('./aws');
-    system("heroku config:set KEY=" + aws.key, function() {
-      grunt.log.writeln("Set key");
-      system("heroku config:set SECRET=" + aws.secret, function() {
-        grunt.log.writeln("Set secret");
-        system("heroku config:set BUCKET=" + aws.bucket, function() {
-          grunt.log.writeln("Set all config params. App is ready to run.");
+    system('heroku config:set KEY=' + aws.key, function() {
+      grunt.log.writeln('Set key');
+      system('heroku config:set SECRET=' + aws.secret, function() {
+        grunt.log.writeln('Set secret');
+        system('heroku config:set BUCKET=' + aws.bucket, function() {
+          grunt.log.writeln('Set all config params. App is ready to run.');
           signal();
         });
       });
@@ -147,16 +149,16 @@ config.heroku = {
       var done = grunt.task.current.async();
       config.heroku.prereq(grunt);
 
-      grunt.log.writeln("Creating Heroku app called: " + appName);
-      system("heroku create " + appName, function(err, result, code) {
+      grunt.log.writeln('Creating Heroku app called: ' + appName);
+      system('heroku create ' + appName, function(err, result, code) {
         if (code === 0) {
-          grunt.log.ok("Created app.");
+          grunt.log.ok('Created app.');
           config.heroku.setConfig(grunt, done);
         } else {
-          grunt.fail.fatal("Heroku app create failed: " + result);
+          grunt.fail.fatal('Heroku app create failed: ' + result);
         }
       });
-      grunt.log.writeln("After spawn.");
+      grunt.log.writeln('After spawn.');
     };
   },
 
